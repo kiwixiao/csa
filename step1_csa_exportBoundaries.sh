4d4828c2fd87eeaf4fd05c746e491593783d3fb0
kiwixiao <qiweixiao@yahoo.com>
2023-03-21 12:40:52 -0400

    second commit, for testing only

--- step1_csa_exportBoundaries.sh
+++ step1_csa_exportBoundaries.sh
@@ -6,9 +6,22 @@ echo "This script runs Starccm+ in batch mode"
 echo "Please make sure your final simulation saved as a name with 'final' in it, so the script\n will locate it precisely without issue."
 echo "This script is adjusted for Inpsire project, maybe not working directly for other projects."
 read -e -p "what is the subject ID: e.g. Inspire02 or Inspire03 etc: " sub
-read -e -p "what is the image number of this subject: e.g.: CT003 or CT006 etc: " category
-read -e -p "First macro to run by default is exporting boundaries from simulation file\n: (input is optional, it has default value) " macro
-read -e -p "The second macro need to run is exploting star data plots from simulation file to allow\n analysis of simulation outputs." macro2
+#read -e -p "First macro to run by default is exporting boundaries from simulation file\n: (input is optional, it has default value) " macro
+#read -e -p "The second macro need to run is exploting star data plots from simulation file to allow\n analysis of simulation outputs." macro2
+
+if echo $sub | grep -q "Inspire"; then
+	echo "This is for Inspire project."
+	read -e -p "what is the image number of this subject: e.g.: CT003 or CT006 etc: " category
+elif echo $sub | grep -q "OSAMRI"; then
+	echo "This is for OSAMRI project"
+	echo "Will set the category variable to empty for compatibility"
+	category=""
+else
+	echo not correct input for subject name input.
+	echo existing code with 1
+	exit 1
+fi
+
 
 macro="${macro:=./functions/exportBoundaryForCSA.java}"
 macro2="${macro2:=./functions/ExportStarPlotData.java}"
@@ -17,7 +30,6 @@ macro2="${macro2:=./functions/ExportStarPlotData.java}"
 sed -i "s/private\ String\ sub\ = .*;/private\ String\ sub\ =\ \"$sub\";/" $macro
 # 		replace the category string using the input data: export boundary macro
 sed -i "s/private\ String\ category\ = .*;/private\ String\ category\ =\ \"$category\";/" $macro
-
 
 #		replace the subject name with correct subject name in: ExportStarPlotData macro
 sed -i "s/private\ String\ subject\ = .*;/private\ String\ subject\ =\ \"$sub\";/" $macro2
